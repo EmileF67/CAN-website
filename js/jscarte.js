6f1e631c5411f88355eacb6ff0fcf76bf239c045
yherlin67 <yanis.herlin@gmail.com>
2025-12-17 13:44:10 +0100

    Avancement cartes

--- js/jscarte.js
+++ js/jscarte.js
@@ -1,3 +1,7 @@
+var passagers = []
+var index = 0
+
+
 const getUneReservation = async (nb) => {
     try{
 
@@ -46,24 +50,20 @@ const getUneReservation = async (nb) => {
                                 <section class='ligne'>
                                     <p class='cart2'>Passager </p> <img id="qrcode" src="../img/qrcode.png" alt="qrcode">
                                 </section>
-                                <section class='ligne'>
-                                    <p class='cart2'>Nom </p> <p class='cart2g'>&nbsp; ${data.nom}</p>
-                                </section>
-                                <section class='ligne'>
-                                    <p class='cart2'>Prénom </p> <p class='cart2g'>&nbsp; ${data.nom}</p>
-                                </section>
-                                <section class='ligne'>
-                                    <p class='cart2'>Catégorie </p> <p class='cart2g'>&nbsp; ${data.nom}</p>
-                                </section>
-                                <section class='ligne'>
-                                    <p class='cart2'>Prix </p> <p class='cart2g'>&nbsp; ${data.nom}</p>
-                                </section>
+                                    <div id='ContainerPassager'>
+                                    </div>
                             </section>
                         </section>
                     </section>`
 
         ContainerReservations.innerHTML = codeHTML
 
+        for(let i = 0; i < data.nbPassagers; i ++)
+		{
+			await getUnPassager(nb, i+1);
+		}
+
+        afficherPassager(0)
 
     } catch (error){ // Gérer les erreurs
         console.error('Une erreur est survenue :', error)
@@ -71,8 +71,70 @@ const getUneReservation = async (nb) => {
     }
 };
 
+//On remplit la liste passagers
+const getUnPassager = async (nbr, nbp) => {
+        try {
+            const response = await fetch(
+                `https://can.iutrs.unistra.fr/api/reservation/${nbr}/passager/${nbp}`
+            );
+
+            if (!response.ok) {
+                throw new Error("Erreur réseau !");
+            }
+
+            const data = await response.json();
+
+            //On ajoute le passager dans le tableau passagers
+            passagers.push(data)
+
+        } catch (error) {
+            console.error("Une erreur est survenue :", error);
+    }
+};
+
+//On affiche le passager
+function afficherPassager(index) {
+    const p = passagers[index];
+
+    const ContainerPassager = document.getElementById('ContainerPassager');
+
+    let codeHTML = `<section class='ligne'>
+                            <p class='cart2'>Nom </p> <p class='cart2g'>&nbsp; ${data.nom}</p>
+                        </section>
+                        <section class='ligne'>
+                            <p class='cart2'>Prénom </p> <p class='cart2g'>&nbsp; ${data.prenom}</p>
+                        </section>
+                        <section class='ligne'>
+                            <p class='cart2'>Catégorie </p> <p class='cart2g'>&nbsp; ${data.libelleCategorie}</p>
+                        </section>
+                        <section class='ligne'>
+                            <p class='cart2'>Prix </p> <p class='cart2g'>&nbsp; ${data.price}</p>
+                        </section>`
+
+    ContainerPassager.innerHTML = codeHTML
+
+    document.getElementById("avant").disabled = index === 0;
+    document.getElementById("apres").disabled = index === passagers.length - 1;
+}
+
+
+
 let valider = document.getElementById("bout")
 valider.addEventListener("click", function() {
     const valeur = document.getElementById("nom").value;
     getUneReservation(valeur)
+});
+
+document.getElementById("avant").addEventListener("click", () => {
+  if (index > 0) {
+    index -= 1;
+    afficherPassager(index);
+  }
+});
+
+document.getElementById("apres").addEventListener("click", () => {
+  if (index < passagers.length - 1) {
+    index += 1;
+    afficherPassager(index);
+  }
 });
